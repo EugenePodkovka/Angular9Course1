f5fae6f037d01e48cc22e6593e3f702fb532d615
Grizo <eugene.podkovka@gmail.com>
2020-05-25 10:10:48 +0300

    Modified AdminProductEdit. Editing products is finished.

--- src/app/admin/components/admin-product-edit/admin-product-edit.component.ts
+++ src/app/admin/components/admin-product-edit/admin-product-edit.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product } from 'src/app/shared/interfaces/product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductServiceService } from 'src/app/product/services/product-service.service';
+import { CartService } from 'src/app/cart/services/cart.service';
+
+enum ProductSearchStatus {
+  Undefined,
+  Found,
+  NotFound
+}
 
 @Component({
   selector: 'app-admin-product-edit',
   templateUrl: './admin-product-edit.component.html',
   styleUrls: ['./admin-product-edit.component.scss']
 })
-export class AdminProductEditComponent implements OnInit {
+export class AdminProductEditComponent {
+  product: Product = {} as Product;
+  testValue: string;
+  productIdUrlParam: string;
+  public productSearchStatus: ProductSearchStatus = ProductSearchStatus.Undefined;
+  productTotalRemaining: number;
+  isAllProductDataLoaded: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductServiceService,
+    private cartService: CartService
+  ) {
+    this.setProduct((p: Product) => {
+      this.setTotalRemaining(p, () => {
+        this.isAllProductDataLoaded = this.productSearchStatus === ProductSearchStatus.Found;
+      });
+    });
   }
 
+  onSaveClick() {
+    this.productService.updateProductInStock(this.product, this.productTotalRemaining);
+    this.cartService.savePurchasedProducts();
+  }
+
+  setProduct(callback?: (p: Product) => any) {
+    this.productIdUrlParam = this.route.snapshot.paramMap.get('id');
+    this.productService.getProduct(this.productIdUrlParam).subscribe((p) => {
+      this.product = p;
+      this.productSearchStatus = p ? ProductSearchStatus.Found : ProductSearchStatus.NotFound;
+      callback?.call(this, p);
+    });
+
+    // TODO why this does not work?
+    // this.productService.getProduct(productId).toPromise().then(val => console.log(val));
+  }
+
+  setTotalRemaining(p: Product, callback?: () => any) {
+    if (this.productSearchStatus === ProductSearchStatus.Found) {
+      this.productService.getProductTotalRemaining(p.Id).subscribe((totalRemaining => {
+        this.productTotalRemaining = totalRemaining;
+        callback?.call(this);
+      }));
+    }
+  }
 }
